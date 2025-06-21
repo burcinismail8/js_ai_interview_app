9a662d394b5dd4ee7d0fefbc0103b5be9f2f7770
burcinismail8 <burcinismail8@gmail.com>
2025-06-21 11:18:36 +0300

    Firebase setup

---
+++ firebase/client.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrO8M6WQX5eluOyMJWdM41e2xOJaeVroo",
+  authDomain: "prepwise---ai-interview.firebaseapp.com",
+  projectId: "prepwise---ai-interview",
+  storageBucket: "prepwise---ai-interview.firebasestorage.app",
+  messagingSenderId: "663787644821",
+  appId: "1:663787644821:web:4679caa679a5f66d54389c",
+  measurementId: "G-B1HVCFN2QR",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
